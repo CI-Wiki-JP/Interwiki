05ba58ec5e2ec3c0a7fb5f3e088076536a9cd4f7
OntakiYuki/温焚雪 <ontaki@outlook.jp>
2026-05-21 17:43:30 +0900

    Add ciBranches variable with language information

---
+++ js/branches-info-ci.js
@@ -0,0 +1,44 @@
+export var ciBranches = {
+  jp: {
+    name: "日本語",
+    head: "他言語版",
+    url: "https://ci-jp.wikidot.com/",
+    id: "5287497",
+    category: "",
+  },
+  en: {
+    name: "English",
+    head: "In other languages",
+    url: "https://ci-wiki.wikidot.com/",
+    id: "623373",
+    category: "",
+  },
+  cn: {
+    name: "中文",
+    head: "其他语言",
+    url: "https://ci-cn-wiki.wikidot.com/",
+    id: "4429215",
+    category: "",
+  },
+  fr: {
+    name: "Français",
+    head: "Dans d’autres langues",
+    url: "https://insurrection-du-chaos.wikidot.com/",
+    id: "1434451",
+    category: "",
+  },
+  es: {
+    name: "Español",
+    head: "En otros idiomas",
+    url: "https://caosinsurgente.wikidot.com/",
+    id: "640728",
+    category: "",
+  },
+  int: {
+    name: "International",
+    head: "Languages",
+    url: "https://ci-wiki-intl.wikidot.com/",
+    id: "4468581",
+    category: "",
+  },
+};
